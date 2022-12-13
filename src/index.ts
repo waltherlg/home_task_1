86f34c037c2d2f059014f5d199a3aaf0b1c133e7
Ruslan <rusishss@mail.ru>
2022-12-13 18:54:17 +0600

    пробую исправить коды ошибко на DELETE

--- src/index.ts
+++ src/index.ts
@@ -105,15 +105,11 @@ app.delete('/videos/:id', (req, res) => {
     for (let i = 0; i < videos.length; i++){
         if (videos[i].id === +req.params.id){
             videos.splice(i, 1);
-            res.status(204);
+            res.send(204);
             return;
         }
-        else {
-            res.status(404);
-        }
-
     }
-
+    res.send(404);
 
 })
 
